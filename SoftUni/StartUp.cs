2462ed94b16bc6dcfeae44a4f2c1f474f9e995f8
Paf <pavlinpenevv@abv.bg>
2022-06-24 23:40:05 +0300

    Solved Tasks
    Committed 4 resolved problems

---
+++ SoftUni/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var softUniContext = new SoftUniContext();
+            //Console.WriteLine(GetEmployeesFullInformation(softUniContext));
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(softUniContext));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees.ToList();
+
+            var sb = new StringBuilder();
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+            sb.ToString().TrimEnd();
+
+            return sb.ToString();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context
+                            .Employees
+                            .Where(x => x.Salary > 50000)
+                            .OrderBy(x => x.FirstName)
+                            .ToList();
+
+            var sb = new StringBuilder();
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+            sb.ToString().TrimEnd();
+
+            return sb.ToString();
+        }
+    }
+}
